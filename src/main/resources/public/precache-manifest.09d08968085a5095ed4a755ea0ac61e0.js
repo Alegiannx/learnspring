f94de90ab43a25f568e50e0e66e7d7e8a69f2d9d
Alegiannx <alegiannx@gmail.com>
2020-09-06 21:04:59 +0300

    major build changes, both front and back end can now be ran using only one bootRun command on the gradlew file. Root now serves index.html

---
+++ src/main/resources/public/precache-manifest.09d08968085a5095ed4a755ea0ac61e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d71755f2aa53097577bf72b7393e28d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ccdd326e4cb626b9364e",
+    "url": "./static/css/main.4f172a94.chunk.css"
+  },
+  {
+    "revision": "92f0eea9c34b3eef7385",
+    "url": "./static/js/2.1998cff6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.1998cff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdd326e4cb626b9364e",
+    "url": "./static/js/main.fbb974c2.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
